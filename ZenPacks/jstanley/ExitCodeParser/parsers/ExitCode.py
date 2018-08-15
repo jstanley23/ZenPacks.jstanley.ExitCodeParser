a94ee432ee330429635a5e5f8bd9a87c9166257b
Jay Stanley <jstanley734@gmail.com>
2018-08-15 17:07:16 -0400

    Enforce severity min/max

--- ZenPacks/jstanley/ExitCodeParser/parsers/ExitCode.py
+++ ZenPacks/jstanley/ExitCodeParser/parsers/ExitCode.py
@@ -8,6 +8,8 @@ class ExitCode(CommandParser):
         exitCode = cmd.result.exitCode
         if exitCode > 5:
             exitCode = 5
+        elif exitCode < 0:
+            exitCode = 0
 
         result.events.append(
             dict(
